edf1f09490c70b7c41fd79537d70020407da1c13
sam-forster <978178@swansea.ac.uk>
2019-11-21 14:22:07 +0000

    Started working on GameManager class

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,4 +1,33 @@
 
+/**
+ * GameManager.java
+ * Controls flow of game and handles Player interaction with the world
+ * @version 0.1
+ * @author Sam Forster
+ * @author ...
+ * @author ...
+ */
+
+
 public class GameManager {
-	//Create Game Manager
+	private Player player;
+	private Board board;
+	private String boardFile;
+	private int gameState;
+	
+	
+	
+	
+	
+	////////////////////////////////////
+	
+	public GameManager () {
+		this.player = new Player();
+		this.board = new Board();
+	}
+	
+	//Waits for a key input and updates the game according to the input
+	public void updateGame() {
+		
+	}
 }
