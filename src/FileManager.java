23ce4c43a2d8a220c0f653cb6fee8c3ae04bce26
Jack Maloney <jackpmaloney2@gmail.com>
2019-11-22 14:54:18 +0000

    Added profile reading class

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -13,7 +15,7 @@ import java.util.Scanner;
 public class FileManager {
 
     public static void main(String[] args) {
-        readMapFile(createFileScanner("assets/Level Example.txt"));
+        readPlayerFile(createFileScanner("assets/Player Example.txt"));
     }
 
     /**
@@ -130,11 +132,124 @@ public class FileManager {
         for (int i = 0; i < 3; i++) {
             String[] player = currentLine.split(",");
             String playerName = player[0];
-            String playerTime = player[1];
+            int playerTime = Integer.parseInt(player[1]);
             System.out.println(playerName + " completed the level in: " + playerTime);
             if (i < 2) {
                 currentLine = in.nextLine();
             }
         }
+    }
+    //REMEMBER TO UPDATE WITH READMAPFILE
+    /**
+     * Generates a player and board from a save file
+     * @param in Scanner of contents of save file
+     */
+    private static void readPlayerFile (Scanner in) {
+        int boardX = in.nextInt();
+        int boardY = in.nextInt();
+        List<String> inventory = new ArrayList<>();
+        int tokenCount = 0;
+        in.nextLine();
+        for (int i = 0; i < boardY; i++) {
+            String currentTileLine = in.nextLine();
+            for (int j = 0; j < (boardX); j++) {
+                String current = "" + currentTileLine.charAt(j);
+                switch (current) {
+                    case "#":
+                        System.out.print("#");
+                        break;
+                    case ".":
+                        System.out.print(".");
+                        break;
+                    case "F":
+                        System.out.print("F");
+                        break;
+                    case "W":
+                        System.out.print("W");
+                        break;
+                    case "T":
+                        System.out.print("T");
+                        break;
+                    case "D":
+                        System.out.print("D");
+                        break;
+                    case "G":
+                        System.out.print("G");
+                        break;
+                    default:
+                        System.out.print("?");
+                }
+            }
+            System.out.println("");
+        }
+        String currentLine = in.nextLine();
+        while (!currentLine.startsWith("CURRENTTIME")) {
+            Scanner line = new Scanner(currentLine).useDelimiter(",");
+            int posX = line.nextInt();
+            int posY = line.nextInt();
+            System.out.println(posX + ", " + posY);
+            String keyword = line.next();
+            switch (keyword) {
+                case "START":
+                    System.out.println("START");
+                    break;
+                case "ITEM":
+                    String itemType = line.next();
+                    if (itemType.equals("TOKEN")) {
+                        int tokenValue = line.nextInt();
+                        System.out.println("It's a token worth: " + tokenValue);
+                    } else {
+                        System.out.println("It's a: " + itemType);
+                    }
+                    break;
+                case "DOOR":
+                    String doorType = line.next();
+                    if (doorType.equals("TOKEN")) {
+                        int tokensRequired = line.nextInt();
+                        System.out.println("It's a door that uses " + tokensRequired + " tokens!");
+                    } else {
+                        String doorColour = line.next();
+                        System.out.println("It's a " + doorColour.toLowerCase() + "door!");
+                    }
+                    break;
+                case "TELE":
+                    int pairValue = line.nextInt();
+                    System.out.println("It's a teleporter part of pair " + pairValue);
+                    break;
+                case "ENEMY":
+                    String enemyType = line.next();
+                    switch (enemyType) {
+                        case "STRAIGHT":
+                            String direction = line.next();
+                            System.out.println("Straight type enemy starting: " + direction);
+                            break;
+                        default:
+                            System.out.print("I haven't added this enemy type to the filereader!");
+                    }
+                    break;
+                default:
+                    System.out.println("Not a recognised keyword!");
+            }
+            currentLine = in.nextLine();
+            line.close();
+        }
+//        String[] currentTimeString = currentLine.split(",");
+        int currentTime = Integer.parseInt(currentLine.split(",")[1]);
+        int playerLevel = Integer.parseInt(in.nextLine().split(",")[1]);
+        System.out.println(in.nextLine());
+
+        while (in.hasNextLine()) {
+            Scanner line = new Scanner(in.nextLine()).useDelimiter(",");
+            String itemType = line.next();
+            inventory.add(itemType);
+            if (itemType.equals("TOKEN")) {
+                tokenCount += line.nextInt();
+            }
+        }
+        for (int i = 0; i < tokenCount; i++) {
+            inventory.add("TOKEN");
+        }
+
+        System.out.println(inventory.toString());
     }
 }
