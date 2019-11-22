72a77621f7bd635ab60d9715c5246eabaec50976
sam-forster <978178@swansea.ac.uk>
2019-11-22 14:23:05 +0000

    Create button to launch game

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -19,167 +20,136 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-  // The dimensions of the window
-  private static final int WINDOW_WIDTH = 1080;
-  private static final int WINDOW_HEIGHT = 600;
+	// The dimensions of the window
+	private static final int WINDOW_WIDTH = 1080;
+	private static final int WINDOW_HEIGHT = 600;
 
-  // The dimensions of the canvas
-  private static final int CANVAS_WIDTH = 320;
-  private static final int CANVAS_HEIGHT = 320;
+	// The dimensions of the canvas
+	private static int CANVAS_WIDTH = 320;
+	private static int CANVAS_HEIGHT = 320;
+	
+	// The size of each cell
+	private static int GRID_CELL_WIDTH = 32;
+	private static int GRID_CELL_HEIGHT = 32;
+	
+	private Canvas canvas; //GUI Canvas
+	
+	// Loaded images
+	Image player;
+	Image dirt;
+	
+	// X and Y coordinate of player
+	int playerX = 0;
+	int playerY = 0;
 
-  // The size of each cell
-  private static int GRID_CELL_WIDTH = 32;
-  private static int GRID_CELL_HEIGHT = 32;
+//////////////////////////////////////////////////////////////////////////////////////////
 
-  //GUI Canvas
-  private Canvas canvas;
+	public void start(Stage primaryStage) {
+		Pane menu = buildMenuGUI();// Build the GUI
+		Scene scene = new Scene(menu, WINDOW_WIDTH, WINDOW_HEIGHT); // Create a scene from the GUI
+		
+		// Load images
+		player = new Image("player.png");
+		dirt = new Image("StoneBrickWall + Ceiling.png");
+		
+		scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> processKeyEvent(event)); // Register an event handler for key presses
+		
+		// Display the scene on the stage
+		drawGame();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		}
 
-  // Loaded images
-  Image player;
-  Image dirt;
+	//Processing the event when a key is pressed
+	public void processKeyEvent(KeyEvent event) {
+		switch (event.getCode()) {
+			// Right key was pressed
+			case RIGHT:
+				playerX = playerX + 1;
+				break;
+			// Left key was pressed
+			case LEFT:
+				playerX = playerX - 1;
+				break;
+			// Up key was pressed
+			case UP:
+				playerY = playerY + 1;
+				break;
+			// Down key was pressed
+			case DOWN:
+				playerY = playerY - 1;
+				break;
+			default:
+				//Do Nothing
+				break;
+		}
 
-  // X and Y coordinate of player
-  int playerX = 0;
-  int playerY = 0;
+		// Redraw game as the player may have moved.
+		drawGame();
 
-  public void start(Stage primaryStage) {
-    // Build the GUI
-    Pane root = buildGUI();
+		// Consume the event. This means we mark it as dealt with. This stops other GUI nodes (buttons etc) responding to it.
+		event.consume();
+	}
 
-    // Create a scene from the GUI
-    Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+	//Draw game on canvas
+	public void drawGame() {
+		// Get the Graphic Context of the canvas. This is what we draw on.
+		GraphicsContext gc = canvas.getGraphicsContext2D();
 
-    // Load images
-    player = new Image("player.png");
-    dirt = new Image("StoneBrickWall + Ceiling.png");
+		// Clear canvas
+		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		
+		// Draw row of dirt images
+		// We multiply by the cell width and height to turn a coordinate in our grid into a pixel coordinate.
+		// We draw the row at y value 2.
+		for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 10; y++) {
+				gc.drawImage(dirt, x * GRID_CELL_WIDTH, y * GRID_CELL_HEIGHT);
+			}
+		}
 
-    // Register an event handler for key presses
-    scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> processKeyEvent(event));
+		// Draw player at current location
+		gc.drawImage(player, playerX * GRID_CELL_WIDTH, playerY * GRID_CELL_HEIGHT);
+	}
 
-    // Display the scene on the stage
-    drawGame();
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
+	//restart the game
+	public void restartGame() {
+		// We just move the player to cell (0, 0)
+		playerX = 0;
+		playerY = 0;
+		drawGame();
+	}
 
-  /**
-   * Process a key event due to a key being pressed, e.g., to the player.
-   *
-   * @param event The key event that was pressed.
-   */
-  public void processKeyEvent(KeyEvent event) {
-    switch (event.getCode()) {
-    
-      case RIGHT:
-        // Right key was pressed. So move the player right by one cell.
-        playerX = playerX + 1;
-        break;
-        // Left key was pressed. So move the player left by one cell.
-      case LEFT:
-    	  playerX = playerX - 1;
-    	  break;
-    	// Up key was pressed. So move the player up by one cell.
-      case UP:
-    	  playerY = playerY + 1;
-    	  break;
-    	// Down key was pressed. So move the player down by one cell
-      case DOWN:
-    	  playerY = playerY - 1;
-    	  break;
-      default:
-        // Do nothing
-        break;
-    }
+//Move player to (approximately) middle of the canvas
+	public void movePlayerToCenter() {
+		// Moving to center of canvas
+		playerX = (int) ((canvas.getWidth()/32)/2);
+		playerY = (int) ((canvas.getHeight()/32)/2);
+		drawGame();
+	}
 
-    // Redraw game as the player may have moved.
-    drawGame();
+	//Create the GUI
+	private Pane buildMenuGUI() {
+		// Create top-level panel that will hold all GUI
+		BorderPane root = new BorderPane();
+		
+		// Create canvas
+		canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+		root.setCenter(canvas);
+		
+		//Button to launch game
+		Button launchButton = new Button("Launch Game");
+		root.getChildren().add(launchButton);
+	
+		// Add button event handlers
+		launchButton.setOnAction(e -> {
+			System.out.println("TRIGGERED");
+		});
+	
+		return root;
+	}
 
-    // Consume the event. This means we mark it as dealt with. This stops other GUI nodes (buttons etc) responding to it.
-    event.consume();
-  }
-
-  /**
-   * Draw the game on the canvas.
-   */
-  public void drawGame() {
-    // Get the Graphic Context of the canvas. This is what we draw on.
-    GraphicsContext gc = canvas.getGraphicsContext2D();
-
-    // Clear canvas
-    gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-    // Draw row of dirt images
-    // We multiply by the cell width and height to turn a coordinate in our grid into a pixel coordinate.
-    // We draw the row at y value 2.
-    for (int x = 0; x < 10; x++) {
-    	for (int y = 0; y < 10; y++) {
-    		gc.drawImage(dirt, x * GRID_CELL_WIDTH, y * GRID_CELL_HEIGHT);
-    	}
-    }
-
-    // Draw player at current location
-    gc.drawImage(player, playerX * GRID_CELL_WIDTH, playerY * GRID_CELL_HEIGHT);
-  }
-
-  /**
-   * Restart the game.
-   */
-  public void restartGame() {
-    // We just move the player to cell (0, 0)
-    playerX = 0;
-    playerY = 0;
-    drawGame();
-  }
-
-  /**
-   * Move the player to roughly the center of the grid.
-   */
-  public void movePlayerToCenter() {
-    // Moving to center of canvas
-    playerX = (int) ((canvas.getWidth()/32)/2);
-    playerY = (int) ((canvas.getHeight()/32)/2);
-    drawGame();
-  }
-
-  /**
-   * Create the GUI.
-   *
-   * @return The panel that contains the created GUI.
-   */
-  private Pane buildGUI() {
-    // Create top-level panel that will hold all GUI
-    BorderPane root = new BorderPane();
-
-    // Create canvas
-    canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-    root.setCenter(canvas);
-
-    // Create a toolbar with some nice padding and spacing
-    HBox toolbar = new HBox();
-    toolbar.setSpacing(10);
-    toolbar.setPadding(new Insets(10, 10, 10, 10));
-    root.setTop(toolbar);
-
-    // Create toolbar content
-    Button restartButton = new Button("Restart");
-    toolbar.getChildren().add(restartButton);
-
-    Button movePlayerToCenterButton = new Button("Center");
-    toolbar.getChildren().add(movePlayerToCenterButton);
-
-    // Add button event handlers
-    restartButton.setOnAction(e -> {
-      restartGame();
-    });
-
-    movePlayerToCenterButton.setOnAction(e -> {
-      movePlayerToCenter();
-    });
-
-    return root;
-  }
-
-  public static void main(String[] args) {
-    launch(args);
-  }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
