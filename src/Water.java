72f50645f97289e837e0864067703b0a8a9a718c
ginosesia <sesiagino@gmail.com>
2019-11-26 23:25:34 +0000

    Updated Fire.java and Water.java
    Added java doc, update method and constructor

--- src/Water.java
+++ src/Water.java
@@ -1,10 +1,28 @@
-
+/**
+ * Water.java Creates a water tile on the board
+ * 
+ * @version 0.3
+ * @author Gino Sesia
+ */
 public class Water extends Tile {
 
+	/**
+	 * Creates a water tile.
+	 * 
+	 * @param x,      the x coordinate of the player.
+	 * @param y,      the y coordinate of the player.
+	 */
 	public Water(int x, int y) {
 		super(x, y, "../assets/Water.png", 1);
 	}
-	
+
+	/**
+	 * Checks if the player is standing on a water tile.
+	 * 
+	 * @param board,      	the board object for the player to be "placed" in.
+	 * @param player, 		the player.
+	 * @param keyboardIn, 	the current keypress.
+	 */
 	public void update(Board board, Player player, int keyboardIn) {
 		if (this.xCoord == player.getPosX() && this.yCoord == player.getPosY()) {
 			if (player.getFlippers() == false) {
