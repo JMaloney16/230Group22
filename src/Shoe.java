c9dea0df07cf215b9e7f151abfc055306c64d03a
Jack Maloney <jackpmaloney2@gmail.com>
2019-11-27 16:41:16 +0000

    Added method to find sprite from type

--- src/Shoe.java
+++ src/Shoe.java
@@ -1,10 +1,33 @@
-
+/**
+ * Shoe.java
+ * Holds information about a key
+ * @see Collectable
+ * @see Interactable
+ * @author Jack Maloney
+ * @version 0.5
+ */
 public class Shoe extends Collectable {
 	private String type;
 
-	public Shoe(int x, int y, String sprite, String type) {
-		super(x, y, sprite, 0);
-		this.type = type;
+	/**
+	 * Creates a new shoe
+	 * @param x X-Coordinate of shoe
+	 * @param y Y-Coordinate of shoe
+	 * @param type Type of shoe (boots/flipper)
+	 */
+	public Shoe(int x, int y, String type) {
+		super(x, y, getSprite(type.toLowerCase()), 0);
+		this.type = type.toLowerCase();
+	}
+
+	private static String getSprite(String type) {
+		String sprite = "../assets/";
+		if (type == "boots") {
+			sprite += "placeholder.png";
+		} else {
+			sprite += "placeholder.png";
+		}
+		return sprite;
 	}
 
 }
