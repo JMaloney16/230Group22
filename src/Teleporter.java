8f165d17e1e43e053eadd27cfa7665dde020b6a1
ginosesia <sesiagino@gmail.com>
2019-11-28 16:52:31 +0000

    Teleporter.java

--- src/Teleporter.java
+++ src/Teleporter.java
@@ -13,9 +13,22 @@ public class Teleporter extends Tile {
 	 * @param x,      the x coordinate of the teleporter tile.
 	 * @param y,      the y coordinate of the teleporter tile.
 	 */
+	private Teleporter partner;
+	
 	public Teleporter(int x, int y) {
 		super(x, y, "assets/Teleporter.png", 0);
 	}
 	
+	public void setPartner(Teleporter partner) {
+		this.partner = partner;
+	}
+	
+	@Override
+	public void update(Board board, Player player, int keyboardIn) {
+		if (this.xCoord == player.getxCoord() && this.yCoord == player.getyCoord()) {
+			player.nextX = player.xCoord;
+			player.nextY = player.yCoord;
+		}
+	}
 
 }
