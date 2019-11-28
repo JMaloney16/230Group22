849f57bd96c2a3b7b3a79b14ab22d148b93cb12f
sam-forster <978178@swansea.ac.uk>
2019-11-28 00:25:44 +0000

    Menumanager
    Removed unused imports

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,15 +1,10 @@
 
 import java.util.ArrayList;
-import java.util.List;
-
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
