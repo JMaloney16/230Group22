2ec8e2ea9799c602b4406f021fccf39580b00b3d
LukeFranciss <p.l.francis@live.co.uk>
2019-12-02 16:51:19 +0000

    updated sound class
    new sound class was missing. Updated so it's there

--- src/SoundEffect.java
+++ src/SoundEffect.java
@@ -3,63 +3,103 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.swing.JOptionPane;
 
 /**
  * SoundEffect.java 
  * A class designed to hold the methods for calling sounds. 
- * Has the ability to start, stop and play sounds on a loop.
- * 
+ * Has the ability to start, stop and play sounds on a loop. 
  * 
  * @version 0.1
  * @author Luke Francis
  *
  */
+
 public class SoundEffect {
+
+	// Used to record the frame position of the track.
+	private long clipTimePosition;
 	
-	Clip clip;
 	
 	/**
-	 * Method used for setting the sound file that is required.
-	 * 
-	 * @param soundFileName, 	The name of the sound file that is required.
+	 * Method for playing the sound on a loop
+	 * @param filepath,		Location of the sound file that is required.
 	 */
-	public void setFile(String soundFileName) {
+	public static void playLoop(String filepath) {
+
+		try {
+			File musicpath = new File(filepath);
+			
+			if(musicpath.exists()) {
+				AudioInputStream audioinput = AudioSystem.getAudioInputStream(musicpath);
+				Clip clip = AudioSystem.getClip();
+				clip.open(audioinput);
+				clip.start();
+				clip.loop(Clip.LOOP_CONTINUOUSLY);
+				
+				JOptionPane.showMessageDialog(null, "Press ok to stop playing");
+			}
+			else {
+				System.out.println("can't find file");
+			}
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Method for playing a single sound.
+	 * @param filepath,		Location of the sound file that is required.
+	 */
+	public static void playSound(String filepath) {
 		
 		try {
-			File file = new File(soundFileName);
+			File musicpath = new File(filepath);
 			
-			AudioInputStream sound = AudioSystem.getAudioInputStream(file);
-			clip = AudioSystem.getClip();
-			clip.open(sound);
-			
+			if(musicpath.exists()) {
+				AudioInputStream audioinput = AudioSystem.getAudioInputStream(musicpath);
+				Clip clip = AudioSystem.getClip();
+				clip.open(audioinput);
+				clip.start();
+				
+				//JOptionPane.showMessageDialog(null, "Press ok to stop playing");
+			}
+			else {
+				System.out.println("can't find file");
+			}
 		}
-		catch(Exception e) {
-			
+		catch(Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 	
 	/**
-	 * Method used for playing a single sound. Sets the sounds frame position to 0
-	 * to play from the start.
-	 */
-	public void playSound() {
-		//clip.setFramePosition(0);
-		clip.start();
-	}
-	
-	/**
-	 * Method used for playing a sound on a loop. Mostly used for background music.
-	 */
-	public void playLoop() {
-//		clip.setFramePosition(0);
-		clip.start();
-		clip.loop(Clip.LOOP_CONTINUOUSLY);
-		
-	}
-	/**
-	 * Method used to stop the sound.
+	 * Method for stopping sounds. ***Not sure if it works yet***
 	 */
 	public void stopSound() {
+		Clip clip = null;
+		try {
+			clip = AudioSystem.getClip();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+		clipTimePosition = clip.getMicrosecondPosition();
 		clip.stop();
+	}
+
+	/**
+	 * Method for resuming sounds ***Not sure if it works yet***
+	 */
+	public void resumeSound() {
+		Clip clip = null;
+		try {
+			clip = AudioSystem.getClip();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+		clip.setMicrosecondPosition(clipTimePosition);
+		clip.start();
 	}
 }
--- src/SoundTest.java
+++ src/SoundTest.java
@@ -1,11 +1,24 @@
 
 public class SoundTest {
 
-	public static void main(String[] args) {
+	
+	/**
+	 * example of how to use SoundEffect.java
+	 */
+	public SoundTest() {
 		SoundEffect test = new SoundEffect();
-		test.setFile("../assests/sounds/Dungeon_004.wav");
 		
-		test.playLoop();
+		String soundplay = "../CS230Group22/230Group22/assets/Sounds/Intro_002.wav";
+		
+		//single play sound
+		//test.playSound(soundplay);
+		//play loop
+		//test.playLoop(soundplay);
+	}
+	
+	
+	public static void main(String[] args) {
+		new SoundTest();
 
 	}
 
