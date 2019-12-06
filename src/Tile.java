2a8b90b45dcb783a58d9e92904ce7d135adc4fba
ginosesia <sesiagino@gmail.com>
2019-12-06 12:09:17 +0000

    update the javadoc comments

--- src/Tile.java
+++ src/Tile.java
@@ -6,6 +6,16 @@
  */
 
 public abstract class Tile extends Interactable {
+	
+	/**
+	 * Creates a tile on the board
+	 * 
+	 * @param x,         x coordinate of the tile
+	 * @param y,         y coordinate of the tile
+	 * @param sprite,    filepath to the image to represnt this tile
+	 * @param blockable, the blocking value of this tile (0 - does not block, 1 -
+	 *                   blocks enemies, 2 - block all)	
+	 */
 	public Tile(int x, int y, String sprite, int blockable) {
 		super(x, y, sprite, blockable);
 	}
