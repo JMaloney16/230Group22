8ae1fe3b7fb61bb1dbfea7cf6e961bfccda7c14a
LukeFranciss <p.l.francis@live.co.uk>
2019-12-08 12:21:28 +0000

    things

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,7 @@ public class Main extends Application {
 	public static void PlaySound() {
 		SoundEffect test = new SoundEffect();
 
-		String soundplay = "assets/Sounds/Dungeon_004.wav";
+		String soundplay = "assets/Sounds/Intro_002.wav";
 
 		// single play sound
 		// test.playSound(soundplay);
